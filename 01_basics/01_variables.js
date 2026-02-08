bf43f2c3358bc22dc9ec45dcc2dc9a73ad0e182c
Aditya Sengar <2k23.csaiml2311761@gmail.com>
2026-02-09 01:56:11 +0530

    var & datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let email = "adi@google.com"
+var accPass= "1234" // fckedup scope
+accCity="kanpur" // can declare but shouldnt, declares it as global
+let accState; // undefined
+
+//accountId = 2 // not allowed to change on const 
+
+email = "a@a.com"
+accPass="qwe"
+accCity="gwalior"
+
+console.log(accountId)
+console.table([accountId,email,accPass,accCity,accState])
