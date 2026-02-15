4c949a122c5e5cefe6f5ac25dfabe13f01b93702
Aditya Sengar <2k23.csaiml2311761@gmail.com>
2026-02-16 00:20:09 +0530

    dates timestamp day formatting

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,22 +1,30 @@
 // Dates
 
 let myDate = new Date()
+
+// date-time print techniques for different format 
+// console.log(myDate);
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
+
+// date & time input technique
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("02-15-2026")
 // console.log(myCreatedDate.toLocaleString());
 
+
+//timestamp dates
+
 let myTimeStamp = Date.now()
 
 // console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(myCreatedDate.getTime()); //millisec
+// console.log(Math.floor(Date.now()/1000)); //seconds
 
 let newDate = new Date()
 console.log(newDate);
@@ -25,7 +33,6 @@ console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
-    weekday: "long",
-    
-})
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",    
+}));
